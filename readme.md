4d83175852477206245015f9af04f441a323ebbb
Jiang liangzhi <lovejianglz@qq.com>
2019-05-04 20:24:21 +0800

    forth commit

--- readme.md
+++ readme.md
@@ -1,19 +1,24 @@
 # yinyangshi
-## YYS自动刷怪脚本
-## 利用adb实现控制安卓手机。
-## 需要根据打怪的速度及手机加载场景的速度调整timer参数
-## 在命令行中使用
-## 使用前需开启手机的开发者模式及USB DEBUG
+* YYS自动刷怪脚本
+* 利用adb实现控制安卓手机。
+* 需要根据打怪的速度及手机加载场景的速度调整timer参数
+* 在命令行中使用
+* 使用前需开启手机的开发者模式及USB DEBUG
+* 本程序仅在华为荣耀7中测试，能正常使用，其余机型涉及屏幕分辨率问题，可能无法正常使用
 使用方法：
-* 启动游戏，进入离岛战斗准备画面
-* 手机USB连接电脑
-* 可选WiFi模式，需手机与电脑在同一局域网下
-* python yinyangshi.py lidao
+1. 下载安装python 3.7
+2. 在python安装目录中打开命令行工具，输入命令 *pip install \project_path\requirement.txt* project_path为项目目录
+3. 启动游戏，手动进入 *离岛* 战斗准备画面
+4. 输入命令 *python \project_path\yinyangshi.py* 启动本程序，根据提示操作
+
 * 需勾选 **取消** 截图分享 
 * 可选参数
     * --btype TEXT     战斗的种类 jinbi yuhun
     * --count INTEGER  战斗次数
-    * --wifi BOOLEAN   是否选用无线连接
+    * --wifi           是否选用无线连接
 
 ## 2019-05-03更新
 * 加入图像匹配，自动根据战斗画面进入下步操作
+## 2019-05-04更新
+* 使用subprocess替换os.system及os.popen
+* 更新配置逻辑
--- yinyangshi.py
+++ yinyangshi.py
@@ -10,6 +10,7 @@ import click
 import re
 import cv2
 import numpy
+import subprocess
 
 
 @click.group()
@@ -21,16 +22,20 @@ def auto_fight():
     x = random.randrange(1600, 1700)
     y = random.randrange(760, 900)
     wait_for_pattern("start_fight.png")
-    os.system(f'adb shell input tap {x} {y}')  # 点击挑战
+    exc_adb_command(f'shell input tap {x} {y}')  # 点击挑战
     # 按战斗按钮后切换到战斗场景的时间
     x = random.randrange(1600, 1850)
     y = random.randrange(760, 930)
     wait_for_pattern("ready.png")
-    os.system(f'adb shell input tap {x} {y}')  # 点击准备
+    exc_adb_command(f'shell input tap {x} {y}')  # 点击准备
+    time.sleep(5)
+    x = random.randrange(866, 880)
+    y = random.randrange(208, 246)
+    exc_adb_command(f'shell input tap {x} {y}')# 点击主怪
     x = random.randrange(1500, 1850)
     y = random.randrange(200, 930)
     wait_for_pattern("end_fight.png")
-    os.system(f'adb shell input tap {x} {y}')  # 点击屏幕，返回
+    exc_adb_command(f'shell input tap {x} {y}')  # 点击屏幕，返回
 
 
 @click.command()
@@ -38,24 +43,66 @@ def auto_fight():
 @click.option("--count", default=20, help="战斗次数", type=int)
 @click.option("--wifi/--usb", default=False, help="是否选用无线连接")
 def lidao(btype, count, wifi):
-    draw_pic("sc.png")
-    if wifi:
-        connect_wireless_device()
-    click.echo(f"Program is running fight {btype}, {count}")
-    while count:
-        auto_fight()
-        count -= 1
-        print(f"剩余 {count} 次")
-    print("自动脚本结束")
+    if connect_device(wifi):
+        click.echo(f"Program is running fight {btype}, {count}")
+        while count:
+            auto_fight()
+            count -= 1
+            print(f"剩余 {count} 次")
+        print("自动脚本结束")
+    exc_adb_command("kill-server")
+    exit(0)
 
 
 def screen_shot():
-    os.system("adb shell screencap -p /sdcard/sc.png")
-    os.popen("adb pull /sdcard/sc.png")
+    exc_adb_command("shell screencap -p /sdcard/sc.png")
+    exc_adb_command("pull /sdcard/sc.png")
+
+
+def exc_adb_command(command):
+    adb = subprocess.Popen(f"adb {command}", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    adb.wait()
+    result = adb.communicate()
+    return result[0].decode("utf-8"), result[1].decode("utf-8")
+
+
+def connect_device(wifi):
+    # todo 1. 用 adb devices查看当前已激活的手机
+    #      2. 若存在 wifi 已连接的直接跳过连接步骤
+    #      3. 检查USB是否连接好
+    #      4. 配置连接WIFI
+    result, error = exc_adb_command("devices")
+    if error:
+        print(error)
+        return False
+    # 先检查设备列表中的第一项是否含有IP地址
+    device = result.split("\r\n")[1]
+    reg = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+    reg = re.compile(reg)
+    result = re.search(reg, device)
+    if result:
+        click.echo("已通过WIFI连接手机")
+        return True
+    # 如果设备列表为空，提示连接手机
+    if "device" not in device:
+        input("请用USB连接你的手机，按回车继续")
+    # 如果选择了WIFI选项，则进入WIFI配置
+    if wifi:
+        if connect_wireless_device():
+            return True
+    # 上述配置结束后，检查设备是否成功连接
+    result, _ = exc_adb_command("devices")
+    if "device" in result.split("\r\n")[1]:
+        return True
+    return False
 
 
 def connect_wireless_device():
-    click.echo("请用USB连接您的手机")
+    exc_adb_command("kill-server")
+    result, _ = exc_adb_command("tcpip 5555")
+    if "restarting in TCP mode port: 5555" not in result:
+        click.echo("请检查手机USB连接或是否已打开开发者模式")
+        return False
     ip = input("确认手机已连接后，输入手机ip地址：\n")
     failed_count = 2
     while not check_ip(ip) and failed_count:
@@ -63,18 +110,14 @@ def connect_wireless_device():
         failed_count -= 1
     if failed_count == 0 and not check_ip(ip):
         print("输入错误次数过多，退出程序!!!")
-        exit(0)
-    os.popen("adb kill-server")
-    result = os.popen("adb tcpip 5555").readlines()[-1]
-    if "restarting in TCP mode port: 5555" not in result:
-        click.echo("请检查手机USB连接或是否已打开开发者模式")
-        exit(0)
+        return False
     input("请断开USB连接，按回车继续")
-    result = os.popen(f"adb connect {ip}").readlines()[-1]
+    result, _ = exc_adb_command(f"connect {ip}")
     if "connected to" not in result:
         click.echo(f"保证手机与电脑在同一局域网内并确认手机IP输入正确\n{ip}")
-        exit(0)
+        return False
     print("手机无线连接成功")
+    return True
 
 
 def check_ip(ip_addr):
@@ -93,7 +136,7 @@ def match_pic(temp, target="sc.png"):
     imgtm = cv2.imread(f"./{temp}")
     res = cv2.matchTemplate(imgsr, imgtm, cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-    print(max_val)
+    # print(max_val)
     if max_val > 0.95:
         return True
     else:
@@ -101,10 +144,11 @@ def match_pic(temp, target="sc.png"):
 
 
 def wait_for_pattern(pattern):
+    screen_shot()
     while not match_pic(pattern):
         screen_shot()
-        time.sleep(2)
-    time.sleep(1 + random.randrange(0, 10, 1)/10)
+        time.sleep(5)
+    time.sleep(1 + random.randrange(0, 10, 1) / 10)
 
 
 def draw_pic(src):
@@ -114,11 +158,13 @@ def draw_pic(src):
 
 yys.add_command(lidao)
 
-
 if __name__ == '__main__':
     yys()
+    # lidao()
+    # print(exc_adb_command("devices").split("\r\n"))#=="List of devices attached\r\n")
     # connect_wireless_device()
+    # connect_device(1)
     # screen_shot()
-    # print(match_pic("3.png"))#,"2 - 副本.png"))
+    # print(match_pic("start_fight.png"))#,"2 - 副本.png"))
     # draw_pic("sc.png")
     # print(wait_for_pattern("end_fight.png"))
